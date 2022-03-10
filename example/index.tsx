97c0b591c54662bc560a2ebcfbd9b80ace6a8b53
Bruno <brunofas12@gmail.com>
2022-03-10 18:09:22 +0800

    core:import dracula css file to index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import CursorChat from '../dist';
-import '../dist/dracula.css';
+import '../dist/styles/dracula.css';
 
 const App = () => {
     return (
