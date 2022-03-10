60977af468332650ac582c237350bb7ec0bdce44
Bruno <brunofas12@gmail.com>
2022-03-10 21:29:12 +0800

    style:add prettier to tsdx.config.js

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,6 +3,7 @@ const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssImport = require('postcss-import');
+const prettier = require('prettier')
 
 module.exports = {
     rollup(config, options) {
@@ -49,8 +50,17 @@ module.exports = {
                     cssnano ({
                         preset:'default'
                     }),
-                    postcssImport()
-                    
+                    postcssImport(),
+                    prettier({
+                        printWidth: 80,
+                        trailingComma: 'es5',
+                        singleQuote: true,
+                        parser: 'typescript',
+                        semi: false,
+                        tabWidth: 4,
+                        useTabs: false,
+
+                    }) 
                 ],
                 inject: false,
                 include: '**/dracula.css',
