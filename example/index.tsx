9f0ad4f79d7688681411b9256b6502cf8efa732c
Bruno <brunofas12@gmail.com>
2022-03-10 21:32:02 +0800

    refactor:refactoring codes

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import CursorChat from '../dist';
-import '../dist/styles/dracula.css';
+import '../dist/dracula.css';
 
 const App = () => {
     return (
