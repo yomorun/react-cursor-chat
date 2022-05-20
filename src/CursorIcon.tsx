bc827040a004fe2f32cddbc973f9d75ecdcec992
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2022-05-20 17:52:31 +0800

    fix: replace cursorIcon to make it appear in the right place (#30)

--- src/CursorIcon.tsx
+++ src/CursorIcon.tsx
@@ -4,19 +4,23 @@ function CursorIcon({ color }: { color: string }) {
     return useMemo(
         () => (
             <svg
-                shapeRendering="geometricPrecision"
+                width="15"
+                height="15"
                 xmlns="http://www.w3.org/2000/svg"
+                shapeRendering="geometricPrecision"
                 fill={color}
             >
-                <path
-                    fill="#666"
-                    d="M9.63 6.9a1 1 0 011.27-1.27l11.25 3.75a1 1 0 010 1.9l-4.68 1.56a1 1 0 00-.63.63l-1.56 4.68a1 1 0 01-1.9 0L9.63 6.9z"
-                />
-                <path
-                    stroke="#fff"
-                    strokeWidth="1.5"
-                    d="M11.13 4.92a1.75 1.75 0 00-2.2 2.21l3.74 11.26a1.75 1.75 0 003.32 0l1.56-4.68a.25.25 0 01.16-.16L22.4 12a1.75 1.75 0 000-3.32L11.13 4.92z"
-                />
+                <g>
+                    <path
+                        d="m0.78682,2.06437a1,1 0 0 1 1.27,-1.27l11.25,3.75a1,1 0 0 1 0,1.9l-4.68,1.56a1,1 0 0 0 -0.63,0.63l-1.56,4.68a1,1 0 0 1 -1.9,0l-3.75,-11.25z"
+                        fill="#666"
+                    />
+                    <path
+                        d="m2.28682,0.08437a1.75,1.75 0 0 0 -2.2,2.21l3.74,11.26a1.75,1.75 0 0 0 3.32,0l1.56,-4.68a0.25,0.25 0 0 1 0.16,-0.16l4.69,-1.55a1.75,1.75 0 0 0 0,-3.32l-11.27,-3.76z"
+                        strokeWidth="1.5"
+                        stroke="#fff"
+                    />
+                </g>
             </svg>
         ),
         [color]
